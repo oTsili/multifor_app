c803b951baa7df4bca28616b901566cfb01220b2
George Vlahavas <vlahavas@gmail.com>
2021-01-13 22:18:56 +0200

    Install the wheels on 1st run too
    Apparently it's needed for the helping_scripts.py script.

--- 1st-run.sh
+++ 1st-run.sh
@@ -4,6 +4,15 @@
 # in order to populate the mongo db that should be running in a
 # different container
 
+# ok, so if a wheel directory is present (mounted as a volume in
+# docker), then install the wheels that are in there and replace the
+# previously installed ones.  That may be necessary since the
+# precompiled tensorflow wheels require AVX instructions and not all
+# CPUs have those
+if [ -d /wheels ]; then
+	pip install -U /wheels/*.whl
+fi
+
 # populate the database
 python helping_scripts.py
 
